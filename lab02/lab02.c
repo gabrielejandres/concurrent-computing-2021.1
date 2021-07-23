4a267aa5962e6b20a89543232648e1f01a772338
Gabriele Jandres Cavalcanti <gabrielejandres@gmail.com>
2021-07-23 00:06:27 -0300

    + comentarios e modularizacao

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -96,21 +96,31 @@ void createThreads(tArgs **args, pthread_t **tid, int size) {
   }
 }
 
-/* Funcao para analisar a corretude da matriz de saida
+/* Funcao para analisar a corretude da matriz de saida. 
+  * Retorna 0 se existem erros ou 1 em caso de estar tudo certo
   * Como as matrizes foram inicializadas com o valor 1 e 2, a matriz de saida deve conter o elemento 1 * 2 * size em todas as posicoes
 */
-void analyzeOutputCorrectness(float **output, int size) {
+int analyzeOutputCorrectness(float **output, int size) {
   float expectedValue = 2 * size;
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
       if ((*output)[i*size + j] != expectedValue) {
         printf("Erro na posicao [%d][%d], valor esperado eh de %lf e o encontrado eh de %lf", i, j, expectedValue, (*output)[i*size + j]);
-        exit(4);
+        return 0;
       }
     }
   }
 
-  printf("Saida correta. Todas as posicoes da matriz de saida conferem. \n");
+  return 1;
+}
+
+// Funcao para liberar a memoria que foi utilizada durante o programa
+void freeMemory(float **firstMatrix, float **secondMatrix, float **output, tArgs **args, pthread_t **tid) {
+  free(*firstMatrix);
+  free(*secondMatrix);
+  free(*output);
+  free(*args);
+  free(*tid);
 }
 
 // Funcao para calcular e exibir o tempo gasto em uma tarefa
@@ -144,20 +154,18 @@ int main(int argc, char* argv[]) {
     nthreads = size;
   }
 
-  // Passo 1. Alocacao de memoria para as matrizes
+  // (a) Alocacao de memoria e inicializacao das matrizes
   GET_TIME(start);
   allocateMemoryToMatrices(&firstInputMatrix, &secondInputMatrix, &outputMatrix, size);
-  showTaskTime(start, "alocacao para matrizes");
-
-  // Passo 2. Inicializacao das matrizes de entrada e saida
-  GET_TIME(start);
   initializeMatrices(&firstInputMatrix, &secondInputMatrix, &outputMatrix, size);
-  showTaskTime(start, "inicializacao das matrizes");
+  showTaskTime(start, "alocacao e inicializacao das matrizes (a)");
 
-  // Passo 3. Multiplicacao das matrizes
+  // Multiplicacao das matrizes (parte concorrente)
 
-  // 3.1. Alocacao das estruturas de dados
+  // (b) Criacao das threads, execucao da multiplicacao e espera pelo termino das threads
   GET_TIME(start);
+
+  // (b) - Parte 1: Alocacao das estruturas de dados
   tid = (pthread_t*) malloc(sizeof(pthread_t)*nthreads);
   if (tid == NULL) {
     puts("Erro de alocacao de memoria para o vetor de identificadores das threads");
@@ -169,26 +177,16 @@ int main(int argc, char* argv[]) {
     puts("Erro de alocacao de memoria");
     return 2;
   }
-  showTaskTime(start, "alocacao das estruturas de dados");
 
-  // 3.2. Cria as threads, executa a multiplicacao e espera pelo termino das threads
-  GET_TIME(start);
+  // (b) - Parte 2: Criacao das threads
   createThreads(&args, &tid, size);
-  showTaskTime(start, "multiplicacao das matrizes");
+  showTaskTime(start, "criacao das threads e execucao da multiplicacao (b)");
 
-  // Passo 4. Corretude da saida
+  // (c) Finalizacao do programa - Teste de corretude da saida e liberacao da memoria
   GET_TIME(start);
-  analyzeOutputCorrectness(&outputMatrix, size);
-  showTaskTime(start, "testes");
-
-  // Passo 5. Liberacao da memoria
-  GET_TIME(start);
-  free(firstInputMatrix);
-  free(secondInputMatrix);
-  free(outputMatrix);
-  free(args);
-  free(tid);
-  showTaskTime(start, "finalizacao");
+  if (analyzeOutputCorrectness(&outputMatrix, size) == 1) printf("Saida correta. Todas as posicoes da matriz de saida conferem. \n");
+  freeMemory(&firstInputMatrix, &secondInputMatrix, &outputMatrix, &args, &tid);
+  showTaskTime(start, "finalizacao (c)");
 
   return 0;
 }
